081a39bfe5149a99ee22c2e3390df1a29ba19be2
Lexi Beavil <alex@aeroniemi.com>
2023-09-26 13:03:32 +0100

    Fixed exports

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,1 +1,2 @@
-export {};
+export { Inky, Colour, Palette } from "./inky";
+export { Inky_ac as Impression73 } from "./inky_ac";
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,2 @@
-import { Inky } from "./inky";
-import { Inky_ac } from "./inky_ac";
-
-module.exports.Inky = Inky
-module.exports.Inky_ac = Inky_ac
+export { Inky, Colour, Palette } from "./inky";
+export { Inky_ac as Impression73 } from "./inky_ac";
